8b49a5a36be8752681c86694ea6968125ee55295
Ian-PB <C00294680@setu.ie>
2026-06-25 23:50:22 +0100

    Changed look of website

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 const menuIcon = document.querySelector('#menu-icon');
 const navLinks = document.querySelector('.nav-links');
+const projectCards = document.querySelectorAll(".project-card");
 
 menuIcon.onclick = () => {
     navLinks.classList.toggle('active');
 }
+
+
+projectCards.forEach((card) => {
+    const images = card.querySelectorAll(".project-images img");
+    let currentIndex = 0;
+
+    setInterval(() => {
+        images[currentIndex].classList.remove("active");
+
+        currentIndex = (currentIndex + 1) % images.length;
+
+        images[currentIndex].classList.add("active");
+    }, 3000);
+});
